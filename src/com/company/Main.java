8b39efa6a3e9acd586a20f2f606950f005aa7c93
starscream202 <starscream202@gmail.com>
2021-03-21 19:03:19 -0500

    dollarSign

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,25 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i <20 ; i++) {
-            System.out.println("Testing");
+        String sentence="25 34 67 89 1000";
+        System.out.println(sentence);
+        String dollarSign="$";
+        String [] strings=sentence.split(" ");
 
+        List<String> strings1=new ArrayList<>();
+
+        for (String words :strings) {
+            String modifiedText=dollarSign+words;
+            strings1.add(modifiedText);
+
+        }
+        for(int i=0;i<strings1.size();i++){
+            System.out.println(strings1.get(i));
         }
     }
 }
